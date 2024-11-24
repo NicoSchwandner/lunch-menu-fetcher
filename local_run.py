dcc849c2809f3678bcbfe3163b31780898c36a9b
Nicolas Schwandner <nico.schwandner@gmail.com>
2024-11-24 17:23:16 +0100

    Add mocking for additional restaurants

--- local_run.py
+++ local_run.py
@@ -2,8 +2,10 @@ import json
 from freezegun import freeze_time
 import responses
 from lambda_function import lambda_handler
-from config import GABYS_MENU_URL
-from local.mock.gabys_html_mock import get_html_text
+from config import GABYS_MENU_URL, BROR_OCH_BORD_MENU_URL, HILDAS_MENU_URL
+from local.mock.gabys_html_mock import get_html_text as get_gabys_html_text
+from local.mock.bror_och_bord_html_mock import get_html_text as get_bror_och_bord_html_text
+from local.mock.hildas_json_mock import get_json_text as get_hildas_json_text
 
 @responses.activate
 def run_local():
@@ -11,9 +13,27 @@ def run_local():
     responses.add(
         responses.GET,
         GABYS_MENU_URL,
-        body=get_html_text(),
+        body=get_gabys_html_text(),
         status=200,
         content_type='text/html'
+    )
+
+    # Mock the request to Bror och Bord menu URL
+    responses.add(
+        responses.GET,
+        BROR_OCH_BORD_MENU_URL,
+        body=get_bror_och_bord_html_text(),
+        status=200,
+        content_type='text/html'
+    )
+
+    # Mock the request to Hilda's menu URL
+    responses.add(
+        responses.GET,
+        HILDAS_MENU_URL,
+        body=get_hildas_json_text(),
+        status=200,
+        content_type='application/json'
     )
 
     # Use freezegun to set the current time to a specific day (e.g., Tuesday)

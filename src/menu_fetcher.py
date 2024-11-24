aaacb574cc0fbf741cd50b448d6db8f56f88224a
Nicolas Schwandner <nico.schwandner@gmail.com>
2024-11-24 17:23:33 +0100

    Add Bror och Bord implementation

--- src/menu_fetcher.py
+++ src/menu_fetcher.py
@@ -1,5 +1,6 @@
 import logging
 
+from src.restaurants.bror_och_bord import get_bror_och_bord_menu_data
 from src.restaurants.gabys import get_gabys_menu_data
 from src.utils.weekday import get_current_weekday
 from src.styling.menu_blocks import build_menu_blocks
@@ -9,25 +10,24 @@ logger.setLevel(logging.INFO)
 
 def get_menu_blocks():
     try:
-        current_weekday = get_current_weekday()
+        current_weekday_english, current_weekday_swedish = get_current_weekday()
 
         # List to hold menu data from all restaurants
         menu_data_list = []
 
         # Get menu data from Gaby's
-        gabys_data, error = get_gabys_menu_data(current_weekday)
+        gabys_data, error = get_gabys_menu_data(current_weekday_english)
         if error:
             logger.error(f"Gaby's: {error}")
         else:
             menu_data_list.append(gabys_data)
 
-        # Future: Add more restaurants here
-        # Example:
-        # another_restaurant_data, error = get_another_restaurant_data(current_weekday)
-        # if error:
-        #     logger.error(f"Another Restaurant: {error}")
-        # else:
-        #     menu_data_list.append(another_restaurant_data)
+        # Get menu data from Bror och Bord
+        bror_och_bord_data, error = get_bror_och_bord_menu_data(current_weekday_swedish)
+        if error:
+            logger.error(f"Bror och Bord: {error}")
+        else:
+            menu_data_list.append(bror_och_bord_data)
 
         if not menu_data_list:
             return None, 'No menu data available.'

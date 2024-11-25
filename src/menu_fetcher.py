2381d3b75e59227d0c04e7150941f9d8424fd4f9
Nicolas Schwandner <nico.schwandner@gmail.com>
2024-11-25 21:32:10 +0100

    Comment out additional restaurants

--- src/menu_fetcher.py
+++ src/menu_fetcher.py
@@ -23,22 +23,22 @@ def get_menu_blocks():
         else:
             menu_data_list.append(gabys_data)
 
-        # Get menu data from Bror och Bord
-        bror_och_bord_data, error = get_bror_och_bord_menu_data(current_weekday_swedish)
-        if error:
-            logger.error(f"Bror och Bord: {error}")
-        else:
-            menu_data_list.append(bror_och_bord_data)
+        # # Get menu data from Bror och Bord
+        # bror_och_bord_data, error = get_bror_och_bord_menu_data(current_weekday_swedish)
+        # if error:
+        #     logger.error(f"Bror och Bord: {error}")
+        # else:
+        #     menu_data_list.append(bror_och_bord_data)
 
-        if not menu_data_list:
-            return None, 'No menu data available.'
+        # if not menu_data_list:
+        #     return None, 'No menu data available.'
 
-        # Get menu data from Hilda's
-        hildas_data, error = get_hildas_menu_data(current_weekday_english)
-        if error:
-            logger.error(f"Hilda's: {error}")
-        else:
-            menu_data_list.append(hildas_data)
+        # # Get menu data from Hilda's
+        # hildas_data, error = get_hildas_menu_data(current_weekday_english)
+        # if error:
+        #     logger.error(f"Hilda's: {error}")
+        # else:
+        #     menu_data_list.append(hildas_data)
 
         # Build blocks for the Slack message
         blocks = build_menu_blocks(menu_data_list)

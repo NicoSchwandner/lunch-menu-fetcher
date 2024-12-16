772b1197e2952cf608543388f95dbbfd07cf3265
Nicolas Schwandner <nico.schwandner@gmail.com>
2024-12-16 10:59:59 +0100

    fix(config): update GABYS_MENU_URL to generic endpoint

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ TIMEZONE = 'Europe/Stockholm'
 RESTAURANT_REQUEST_TIMEOUT = 3.0
 
 # Restaurants
-GABYS_MENU_URL = "https://jacyzhotel.com/en/restaurants/gabys/"
+GABYS_MENU_URL = "https://jacyzhotel.com/restaurants/gabys/"
 BROR_OCH_BORD_MENU_URL = "https://brorochbord.se/"
 HILDAS_MENU_URL = "https://api.hildasrestaurang.se/wp-json/wp/v2/lunch?per_page=1&orderBy=publish_date"
